e4d2c8b1a825bd41300fa8a8ee00e6de0d8c7e03
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-11 14:11:44 -0700

    updated

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const connectDB = require("./config/db");
 const videoRoutes = require("./routes/api/videoRoutes");
 const commentsRoutes = require("./routes/api/commentsRoutes");
 
+// Dev only
+// const result = dotenv.config()
+// if (result.error) {
+//   throw result.error
+// }
+
 
 // initialize app config
 const app = express();
 connectDB();
+
 
 
 // Init Middleware
@@ -19,13 +26,13 @@ app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentsRoutes);
 
 // Static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 
 const PORT = process.env.PORT || 5000;
